a3d1b0efa9de63f6c246c89f8032aa01a43d8475
Manuel <ma.reza29@gmail.com>
2026-09-02 13:42:51 +0200

    Removed logos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
       <MarketsStrip compact />
       <ExpertiseStack compact />
       {/* <CredibilityTeaser /> */}
-      <PartnerLogos />
+      {/* <PartnerLogos /> */}
       <Contact />
     </>
   );
